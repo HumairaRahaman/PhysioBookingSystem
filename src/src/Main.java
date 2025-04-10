6a32c920317bbf54f7e0012aee6bd66d81d05134
hr24aal <hr24aal@herts.ac.uk>
2025-04-10 02:05:41 +0100

    feat: add the person class

---
+++ src/src/Main.java
@@ -0,0 +1,7 @@
+import ui.BookingSystemUI;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/src/model/Person.java
@@ -0,0 +1,61 @@
+package model;
+
+public abstract class Person {
+    private String id;
+    private String name;
+    private String address;
+    private String phoneNumber;
+
+    public Person(String id, String name, String address, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Name: " + name + ", Phone: " + phoneNumber;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Person person = (Person) obj;
+        return id.equals(person.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}
