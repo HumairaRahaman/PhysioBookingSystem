6f7895c8f643e101ebf6c93a8865ec8df4b503de
hr24aal <hr24aal@herts.ac.uk>
2025-04-10 03:16:52 +0100

    feat: add the patient class

---
+++ src/src/model/Patient.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Patient extends Person {
+
+    public Patient(String id, String name, String address, String phoneNumber) {
+        super(id, name, address, phoneNumber);
+    }
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
